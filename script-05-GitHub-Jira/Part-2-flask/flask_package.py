b62192cf1b8b4d39c209e885493de12db0296112
Neha Avasekar <109887502+Mygithubneha@users.noreply.github.com>
2024-06-14 15:26:34 +0000

    adding file

--- script-05-GitHub-Jira/Part-2-flask/flask_package.py
+++ script-05-GitHub-Jira/Part-2-flask/flask_package.py
@@ -7,13 +7,13 @@ import json
 app = Flask(__name__)
 
 #Creating decorators
-@app.route("/createJIRA" .methods=['POST'])
+@app.route("/createJIRA", methods=['POST'])
 def createJIRA():
 
 
     url = "https://nehaavasekar.atlassian.net/rest/api/3/issue"
 
-    API_TOKEN = ""
+    API_TOKEN = "ATATT3xFfGF0SVo_4xj3MOO7Yp_ns0-zA9gFU2aTZ6URQUl6VEmksffSDeClWneApNIyjPkz_Hke_FmZbIRdYep2VlhVFN-H7H6ZKH2ts6Z1xZEl_sTTQSDn4athicwlMzLj4QDwnzTvLHtr7VNkVC_onYays0_CEF-t-n0BrmJoAM5ECf5uDoU=4414EF25"
 
     auth = HTTPBasicAuth("", API_TOKEN)
 
@@ -58,6 +58,6 @@ def createJIRA():
     auth=auth
     )
 
-    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
 
 app.run('0.0.0.', port=5000)

b2f559ce9805ca4bc2f81430724daa66007cd7a1
Neha Avasekar <109887502+Mygithubneha@users.noreply.github.com>
2024-06-14 15:14:56 +0000

    adding part-2 of this program

---
+++ script-05-GitHub-Jira/Part-2-flask/flask_package.py
@@ -0,0 +1,63 @@
+from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+#creating a flask application instance
+app = Flask(__name__)
+
+#Creating decorators
+@app.route("/createJIRA" .methods=['POST'])
+def createJIRA():
+
+
+    url = "https://nehaavasekar.atlassian.net/rest/api/3/issue"
+
+    API_TOKEN = ""
+
+    auth = HTTPBasicAuth("", API_TOKEN)
+
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+    "fields": {
+        "description": {
+        "content": [
+            {
+            "content": [
+                {
+                "text": "My first jira ticket",
+                "type": "text"
+                }
+            ],
+            "type": "paragraph"
+            }
+        ],
+        "type": "doc",
+        "version": 1
+        },
+        "project": {
+        "key": "AB"
+        },
+        "issuetype": {
+        "id": "10006"
+        },
+        "summary": "First JIRA Ticket",
+    },
+    "update": {}
+    } )
+
+    response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers,
+    auth=auth
+    )
+
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
+
+app.run('0.0.0.', port=5000)

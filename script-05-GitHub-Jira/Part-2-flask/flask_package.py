68d80cb260594408b8bd331997d3ab2374f8fcb1
neha <neha.avasekar@outlook.com>
2024-06-15 14:02:24 +0530

    adding files

--- script-05-GitHub-Jira/Part-2-flask/flask_package.py
+++ script-05-GitHub-Jira/Part-2-flask/flask_package.py
@@ -13,9 +13,9 @@ def createJIRA():
 
     url = "https://nehaavasekar.atlassian.net/rest/api/3/issue"
 
-    API_TOKEN = ""
+    API_TOKEN = "ATATT3xFfGF0SVo_4xj3MOO7Yp_ns0-zA9gFU2aTZ6URQUl6VEmksffSDeClWneApNIyjPkz_Hke_FmZbIRdYep2VlhVFN-H7H6ZKH2ts6Z1xZEl_sTTQSDn4athicwlMzLj4QDwnzTvLHtr7VNkVC_onYays0_CEF-t-n0BrmJoAM5ECf5uDoU=4414EF25"
 
-    auth = HTTPBasicAuth("", API_TOKEN)
+    auth = HTTPBasicAuth("neha.avasekar@outlook.com", API_TOKEN)
 
     headers = {
     "Accept": "application/json",
@@ -24,26 +24,12 @@ def createJIRA():
 
     payload = json.dumps( {
     "fields": {
-        "description": {
-        "content": [
-            {
-            "content": [
-                {
-                "text": "My first jira ticket",
-                "type": "text"
-                }
-            ],
-            "type": "paragraph"
-            }
-        ],
-        "type": "doc",
-        "version": 1
-        },
+
         "project": {
         "key": "AB"
         },
         "issuetype": {
-        "id": "10006"
+        "id": "10001"
         },
         "summary": "First JIRA Ticket",
     },
@@ -60,4 +46,5 @@ def createJIRA():
 
     return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
 
-app.run('0.0.0.', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ script-05-GitHub-Jira/Part-2-flask/scrip-output.md
@@ -0,0 +1,19 @@
+# Final output
+
+```python
+Webhook success
+```
+
+![alt text](image-2.png)
+
+```python
+commented /jira on an issue
+```
+
+![alt text](image-1.png)
+
+```python
+Jira ticket has been created automatically
+```
+
+![alt text](image.png)
